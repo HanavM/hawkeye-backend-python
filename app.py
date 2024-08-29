988dcee046b9b33cf9cccb6f07a0006f0ff06f9f
HanavM <104187022+HanavM@users.noreply.github.com>
2024-08-29 00:02:04 -0700

    test 9

--- app.py
+++ app.py
@@ -1,35 +1,46 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from azure.identity import DefaultAzureCredential
-import pyodbc
 import urllib
 
 app = Flask(__name__)
 
+# Use the DefaultAzureCredential for Managed Identity
+credential = DefaultAzureCredential()
+
 # SQL Server details
 server = 'hawkeye-server-test.database.windows.net'
 database = 'hawkeye-DB-test'
-driver = 'ODBC Driver 17 for SQL Server'  # Notice there is no '+' sign
+driver = 'ODBC+Driver+17+for+SQL+Server'
 
 # Build the connection string
-params = urllib.parse.quote_plus(f'DRIVER={{{driver}}};SERVER={server};DATABASE={database};Authentication=ActiveDirectoryMSI;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+params = urllib.parse.quote_plus(
+    f'DRIVER={{{driver}}};'
+    f'SERVER={server};'
+    f'DATABASE={database};'
+    'Authentication=ActiveDirectoryMSI;'
+    'Encrypt=yes;'
+    'TrustServerCertificate=no;'
+    'Connection Timeout=30;'
+)
 
-# Connection string for pyodbc
-connection_string = f"mssql+pyodbc:///?odbc_connect={params}"
+# Set the SQLALCHEMY_DATABASE_URI configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mssql+pyodbc:///?odbc_connect={params}"
+
+# Initialize SQLAlchemy
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
-    return "Hello, World! test: 8"
+    return "Hello, World! test: 3"
 
 @app.route('/create_db')
 def create_db():
     try:
-        # Attempt to connect to the database
-        with pyodbc.connect(connection_string) as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT 1")  # Simple query to test connection
-            return "Connected successfully!", 200
+        db.create_all()
+        return "Database tables created successfully!", 200
     except Exception as e:
-        return f"Error: {e}", 500
+        return str(e), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
