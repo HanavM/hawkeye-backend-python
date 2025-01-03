07db68a76f5a5505d753f15c76452fcd7ad4fdce
HanavM <104187022+HanavM@users.noreply.github.com>
2025-01-02 23:19:43 -0800

    trying again

--- testIGroute.py
+++ testIGroute.py
@@ -2,7 +2,7 @@ from instagram import Instagram
 import json
 
 try:
-    profile_data = Instagram.scrap('ksivaspams')
+    profile_data = Instagram.scrap('sdgjwebgibwevneovnaesovmseovbes')
     profile_data = json.loads(profile_data)
     # print(profile_data)
     full_name = profile_data["full_name"]
@@ -11,6 +11,7 @@ try:
         first_name = name_parts[0]
         last_name = name_parts[1] if len(name_parts) > 1 else ""
     print(first_name + " xxx " + last_name)
+    print("im right herererer")
 except:
     print("well account doesnt exist")
 # print(profile_data)
